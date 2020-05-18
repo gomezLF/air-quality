ba3fd669f8bc28b554a6834ab364a6e0595d2b13
Manuel Balanta <mbalantaq@gmail.com>
2020-05-18 01:31:15 -0500

    Método sumatoria

---
+++ code/model/Algorithms.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model
+{
+    static class Algorithms
+    {
+        public static Double LinearRegresion(List<Double> x, List<Double> y, String Departament) {
+            Double LinearRegresion = 0;
+
+
+            return LinearRegresion;
+        }
+
+        private static Double Summation(int First, int Last, List<Double> Variables) {
+            if (First == Last)
+            {
+                return Variables[First];
+            }
+            else {
+                int Middle = (First + Last) / 2;
+                Double s = Summation(First, Middle, Variables);
+                Double t = Summation(Middle+1, Last, Variables);
+                return s + t;
+            }
+        }
+    }
+}
