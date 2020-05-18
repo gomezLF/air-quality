ff3e4b918a330d8685b4c7d6dc13a17d5c153f10
Manuel Balanta <mbalantaq@gmail.com>
2020-05-18 03:07:15 -0500

    Listo el método de regresión lineal

--- code/model/Algorithms.cs
+++ code/model/Algorithms.cs
@@ -6,26 +6,70 @@ using System.Threading.Tasks;
 
 namespace model
 {
-    static class Algorithms
+    public class Algorithms
     {
-        public static Double LinearRegresion(List<Double> x, List<Double> y, String Departament) {
-            Double LinearRegresion = 0;
+        private Double a;
+        private Double b;
 
-
-            return LinearRegresion;
+        public Algorithms()
+        {
+         
         }
 
-        private static Double Summation(int First, int Last, List<Double> Variables) {
+        public void Coefficients(List<Double> x, List<Double> y, String Departament)
+        {
+            //Double LinearRegresion = 0;
+            Double SumX = Summation(0, x.Count - 1, x);
+            Double SumY = Summation(0, y.Count - 1, y);
+            List<Double> SqX = Squared(x);
+            List<Double> SqY = Squared(y);
+            List<Double> MultXY = Multuply(x, y);
+            Double SumSqX = Summation(0, x.Count - 1, SqX);
+            Double SumSqY = Summation(0, y.Count - 1, SqY);
+            Double SumMXY = Summation(0, MultXY.Count - 1, MultXY);
+            a = ((MultXY.Count * SumMXY) - (SumX * SumY))/((SqX.Count*SumSqX) - Math.Pow(SumSqX, 2));
+            b = ((SumY) - (a * SumX)) /(x.Count);
+            //return LinearRegresion;
+        }
+
+        public Double LinearRegresion(Double x) 
+        {
+            Double y = (a * x) + b;
+            return y;
+        }
+
+        private Double Summation(int First, int Last, List<Double> Variables) {
             if (First == Last)
             {
                 return Variables[First];
             }
-            else {
+            else
+            {
                 int Middle = (First + Last) / 2;
                 Double s = Summation(First, Middle, Variables);
-                Double t = Summation(Middle+1, Last, Variables);
+                Double t = Summation(Middle + 1, Last, Variables);
                 return s + t;
             }
         }
+
+        private List<Double> Squared(List<Double> Variables) {
+            List<Double> sn = new List<double>();
+            for (int i = 0; i < Variables.Count - 1; i++)
+            {
+                sn[i] = Variables[i] * Variables[i];
+            }
+            return sn;
+        }
+
+        private List<Double> Multuply(List<Double> x, List<Double> y)
+        {
+            List<Double> multiply = new List<double>();
+            for (int i = 0; i < x.Count-1; i++)
+            {
+                multiply[i] = x[i] * y[i];
+            }
+            return multiply;
+        }
+
     }
 }
